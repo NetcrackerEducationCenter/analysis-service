3be8925538cec72bfc91e09fd3206f89f228a370
Alexander Sakharov <sakharov487@mail.ru>
2021-03-14 17:36:58 +0300

    Added date and keywords fields to AnalyticsServiceResponse class

--- src/main/java/org/netcracker/learningcenter/utils/AnalyticsServiceResponse.java
+++ src/main/java/org/netcracker/learningcenter/utils/AnalyticsServiceResponse.java
@@ -1,8 +1,12 @@
 package org.netcracker.learningcenter.utils;
 
+import java.util.List;
+
 public class AnalyticsServiceResponse {
     private String requestId;
     private Status status;
+    private List<String> keywords;
+    private String date;
 
     public AnalyticsServiceResponse() {
     }
@@ -10,6 +14,29 @@ public class AnalyticsServiceResponse {
     public AnalyticsServiceResponse(String requestId, Status status) {
         this.requestId = requestId;
         this.status = status;
+    }
+
+    public AnalyticsServiceResponse(String requestId, Status status, List<String> keywords, String date) {
+        this.requestId = requestId;
+        this.status = status;
+        this.keywords = keywords;
+        this.date = date;
+    }
+
+    public List<String> getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
     }
 
     public String getRequestId() {
